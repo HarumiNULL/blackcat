46e649d4de219b21bcd6fe8413ebeab1507e4799
Farfalla <marivale13228@gmail.com>
2025-04-25 00:19:09 -0500

    RF-001 Registro de Usuarios

--- blackcat/Controllers/UserController.cs
+++ blackcat/Controllers/UserController.cs
@@ -1,19 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using blackcat.Models;
+using blackcat.Services;
 
 namespace blackcat.Controllers;
 
 public class UserController : Controller
 {
-    // GET
-    public IActionResult Index()
+    private readonly IAuthService _authService;
+
+    // Inyección del servicio
+    public UserController(IAuthService authService)
     {
-        return View();
+        _authService = authService;
     }
+
+    // GET - Página de registro
     public IActionResult Registro()
     {
         return View();
     }
 
+    // POST - Registrar usuario
+    [HttpPost]
+    public async Task<IActionResult> Registro(Usuario usuario)
+    {
+        var resultado = await _authService.RegistrarUsuarioAsync(usuario);
+        if (resultado == "Usuario registrado correctamente.")
+        {
+            return RedirectToAction("InicioSesion");  // Redirige al login
+        }
+        else
+        {
+            ViewBag.Error = resultado;  // Muestra mensaje de error
+            return View("Registro");  // Vuelve a la vista de registro
+        }
+    }
+
+    // GET - Página de inicio de sesión
     public IActionResult InicioSesion()
     {
         return View();
--- blackcat/Program.cs
+++ blackcat/Program.cs
@@ -1,21 +1,27 @@
 using blackcat.Models;
+using blackcat.Services;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddScoped<IAuthService, RegistroU>();
 
 builder.Services.AddDbContext<BlackcatDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("gato"));
 });
 
+// hasheo de contraseñas
+builder.Services.AddScoped<IPasswordHasher<Usuario>, PasswordHasher<Usuario>>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (!app.Environment.IsDevelopment()) 
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
